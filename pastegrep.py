b19a043f796c848b67a6ae71b5f834fbb962f4bf
Benson Kalahar <bensonk42@gmail.com>
2012-03-10 08:39:54 +0000

    Fixed format strings to support python 2.6

--- pastegrep.py
+++ pastegrep.py
@@ -8,10 +8,10 @@ from os.path import isdir, exists
 outdir = 'pastes'
 
 def save_paste(identifier, data):
-    fname = "{}/paste_{}.txt".format(outdir, identifier)
+    fname = "{0}/paste_{1}.txt".format(outdir, identifier)
     with open(fname, 'w') as out:
         out.write(data)
-    print "Saved {}".format(fname)
+    print "Saved {0}".format(fname)
 
 def pastegrep(expression, cb=save_paste, interval=20, endpoint='http://pastebin.com/ajax/realtime_data.php'):
     """Loops endlessly and grabs the index of recent pastes from pastebin.
@@ -40,19 +40,19 @@ def fetch(expression, url):
                     seen.add(identifier)
 
 def handle_paste(identifier, expression, save_func=save_paste):
-    url = "http://pastebin.com/raw.php?i={}".format(identifier)
+    url = "http://pastebin.com/raw.php?i={0}".format(identifier)
     try:
         data = urlopen(url).read()
         if search(expression, data, I|M):
             save_func(identifier, data)
     except Exception as e:
-        print "Caught an exception: {}".format(e)
-        print "URL was {}".format(url)
+        print "Caught an exception: {0}".format(e)
+        print "URL was {0}".format(url)
 
 if __name__ == "__main__":
     from sys import argv
     if len(argv) <= 1: expression = 'password'
     else: expression = argv[1]
 
-    print "Fetching all pastes that match the regular expression '{}'".format(expression)
+    print "Fetching all pastes that match the regular expression '{0}'".format(expression)
     pastegrep(expression)
